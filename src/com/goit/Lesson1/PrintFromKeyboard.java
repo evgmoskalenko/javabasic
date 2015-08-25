42b392f5301e93ccfad2033d3cde390354493a26
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-08-26 00:14:42 +0300

    print from keyboard

---
+++ src/com/goit/Lesson1/PrintFromKeyboard.java
@@ -0,0 +1,16 @@
+package com.goit.Lesson1;
+
+import java.util.Scanner;
+
+public class PrintFromKeyboard {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your name:");
+        String name = in.nextLine();
+        System.out.println("Please enter your surname:");
+        String surname = in.nextLine();
+        System.out.println("Please enter your age:");
+        int age = in.nextInt();
+        System.out.printf("Your name and surname: " + name + " " + surname + " and your age: " + age);
+    }
+}
