cfc5ed8d4b0a5a562831e3a45225cbfa9c529789
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-09-15 18:04:13 +0300

    lessons 4

---
+++ src/com/goit/Lesson4/ArrayMinMaxValue.java
@@ -0,0 +1,30 @@
+package Lesson4;
+
+public class ArrayMinMaxValue {
+    public static void main(String[] args) {
+        System.out.println("max value: " + min + " and " "min value: " + max +);
+    }
+    public static int min(int[] arrayMin) {
+        int minValue = Integer.MAX_VALUE;
+        if (arrayMin.length != 0) {
+            for (int i = 0; i < arrayMin.length; i++) {
+                if (arrayMin[i] < minValue) {
+                    minValue = arrayMin[i];
+                }
+            }
+            return minValue;
+        } else {
+            System.out.println("¬ведите пожалуйста числа");
+            return 0;
+        }
+    }
+    public static int max(int[] arrayMax) {
+        int maxValue = Integer.MAX_VALUE;
+        for (int i=0; i<arrayMax.length; i++) {
+            if (maxValue < arrayMax[i]) {
+                maxValue = arrayMax[i];
+            }
+        }
+        return maxValue;
+    }
+}
