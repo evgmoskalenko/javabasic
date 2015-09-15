bbafc24acd9a30ceddf654afef45710ccc62f896
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-09-15 18:30:10 +0300

    add task

--- src/com/goit/Lesson4/ArrayMinMaxValue.java
+++ src/com/goit/Lesson4/ArrayMinMaxValue.java
@@ -1,9 +1,15 @@
 package Lesson4;
 
+/**
+ * написать класс который будет находить минимальное и максимальное значение в массиве
+ * сортировка массива по возрастанию и убыванию
+ */
+
 public class ArrayMinMaxValue {
     public static void main(String[] args) {
-        System.out.println("max value: " + min + " and " "min value: " + max +);
+        //System.out.println("max value: " + min + " and " "min value: " + max +);
     }
+
     public static int min(int[] arrayMin) {
         int minValue = Integer.MAX_VALUE;
         if (arrayMin.length != 0) {
@@ -18,13 +24,19 @@ public class ArrayMinMaxValue {
             return 0;
         }
     }
+
     public static int max(int[] arrayMax) {
-        int maxValue = Integer.MAX_VALUE;
-        for (int i=0; i<arrayMax.length; i++) {
-            if (maxValue < arrayMax[i]) {
-                maxValue = arrayMax[i];
+        int maxValue = Integer.MIN_VALUE;
+        if (arrayMax.length != 0) {
+            for (int i = 0; i < arrayMax.length; i++) {
+                if (arrayMax[i] > maxValue) {
+                    maxValue = arrayMax[i];
+                }
             }
+            return maxValue;
+        } else {
+            System.out.println("Введите пожалуйста числа");
+            return 0;
         }
-        return maxValue;
     }
 }
