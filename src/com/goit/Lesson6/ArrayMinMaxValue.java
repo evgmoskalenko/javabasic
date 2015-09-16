3f220f8c0f2cd15fea9e3b67125b677497261887
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-09-17 01:07:19 +0300

    find min and max value

--- src/com/goit/Lesson6/ArrayMinMaxValue.java
+++ src/com/goit/Lesson6/ArrayMinMaxValue.java
@@ -9,18 +9,24 @@ import java.util.Scanner;
 
 public class ArrayMinMaxValue {
     public static void main(String[] args) {
-        min(inputData());
-        max(inputData());
-        //System.out.println("max value: " + min() + " and" + "min value: " + max());
+        int[] foobar = inputData();
+        if (foobar.length != 0) {
+            min(foobar);
+            max(foobar);
+            System.out.println("max value: " + max(foobar) + ", and " + "min value: " + min(foobar));
+        } else {
+            System.out.println("Error");
+        }
         //Arrays.sort()
     }
 
     public static int[] inputData() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter numbers");
+        System.out.println("Enter the length of the array:");
         int s1 = in.nextInt();
+        System.out.println("Input " + s1 + " numbers");
         int[] s = new int[s1];
-        for (int i = 0; i<s1; i++) {
+        for (int i = 0; i < s1; i++) {
             int inputDataNumber = in.nextInt();
             s[i] = inputDataNumber;
         }
@@ -29,31 +35,21 @@ public class ArrayMinMaxValue {
 
     public static int min(int[] arrayMin) {
         int minValue = Integer.MAX_VALUE;
-        if (arrayMin.length != 0) {
-            for (int i = 0; i < arrayMin.length; i++) {
-                if (arrayMin[i] < minValue) {
-                    minValue = arrayMin[i];
-                }
+        for (int i = 0; i < arrayMin.length; i++) {
+            if (arrayMin[i] < minValue) {
+                minValue = arrayMin[i];
             }
-            return minValue;
-        } else {
-            System.out.println("Please enter the numbers");
-            return 0;
         }
+        return minValue;
     }
 
     public static int max(int[] arrayMax) {
         int maxValue = Integer.MIN_VALUE;
-        if (arrayMax.length != 0) {
-            for (int i = 0; i < arrayMax.length; i++) {
-                if (arrayMax[i] > maxValue) {
-                    maxValue = arrayMax[i];
-                }
+        for (int i = 0; i < arrayMax.length; i++) {
+            if (arrayMax[i] > maxValue) {
+                maxValue = arrayMax[i];
             }
-            return maxValue;
-        } else {
-            System.out.println("Please enter the numbers");
-            return 0;
         }
+        return maxValue;
     }
 }
