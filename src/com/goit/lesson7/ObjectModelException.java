23efe4fe215d21713699936d374acbab8aa76d83
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-09-18 16:58:24 +0300

    homework #7

---
+++ src/com/goit/lesson7/ObjectModelException.java
@@ -0,0 +1,10 @@
+package com.goit.lesson7;
+
+/**
+ * Create your own exception class for OO-model from home task "Java and OOP"
+ * Add processing for created exception
+ * (к домашнему заданию, которое было по ООП, добавить свой самописный класс c Exception и обработать его)
+ */
+public class ObjectModelException {
+
+}
