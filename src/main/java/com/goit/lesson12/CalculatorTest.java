b64fc9ec874c1bc79918f622f8dd7a2b3ec16346
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-10-08 23:18:10 +0300

    start create JUnit test in the classroom

---
+++ src/main/java/com/goit/lesson12/CalculatorTest.java
@@ -0,0 +1,45 @@
+package com.goit.lesson12;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        calculator new Calculator();
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(3, calculator().add(1, 2));
+    }
+
+    @Test
+    public void testAddSevenAndFive() {
+        final int arg1 = 7;
+        final int arg2 = 5;
+        assertEquals(arg1 + arg2, calculator.add(arg1, arg2));
+    }
+
+    @Test
+    public void testMult() throws Exception {
+        final int arg1 = 2;
+        final int arg2 = 5;
+        assertEquals(arg1 + arg2, calculator.mult(arg1, arg2));
+    }
+
+//    @Test
+//    public void testDiv() throws Exception {
+//        int delimoe = 42;
+//        int delimoe = 7;
+//
+//        int chastnoe = 42 / 7;
+//
+//        assertEquals(6, calculator.add(arg1, arg2));
+//    }
+}
