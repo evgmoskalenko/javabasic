e8b130874c5c2ef98d4f0920deb1c7574228d74f
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-10-08 22:57:42 +0300

    add Maven for project with JUnit

---
+++ src/main/java/com/goit/lesson12/Calculator.java
@@ -0,0 +1,15 @@
+package com.goit.lesson12;
+
+
+public class Calculator {
+    public int add(final int a, final int b) {
+        return a + b;
+    }
+    public int mult(final int arg1, final int arg2) {
+        return arg1 + arg2;
+    }
+
+    public int div() {
+        return arg1 / arg2;
+    }
+}
